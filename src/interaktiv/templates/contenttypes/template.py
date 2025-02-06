824c2eeca71471359c573f5327a8f8e50467eaa9
Maurice Reim <reim@interaktiv.de>
2025-02-06 16:12:20 +0100

    removed unnecessary behavior and added schema directly in content type

--- src/interaktiv/templates/contenttypes/template.py
+++ src/interaktiv/templates/contenttypes/template.py
@@ -1,10 +1,20 @@
 from zope.interface import implementer
 from plone.app.contenttypes.interfaces import IDocument
 from plone.app.contenttypes.content import Document
+from zope import schema
+
+from interaktiv.templates import _
 
 
 class ITemplate(IDocument):
     """ Interface for Template """
+
+    template_description = schema.TextLine(
+        title=_('Template description'),
+        description=_('give a quick description for the template'),
+        default='',
+        required=False
+    )
 
 
 @implementer(ITemplate)
