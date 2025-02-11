f87fb9ef3476b1bcc3128d0fe7a31085bb9e6d19
Maurice Reim <reim@interaktiv.de>
2025-02-11 16:57:55 +0100

    Refactor Template to use FolderishDocument and update schema

--- src/interaktiv/templates/contenttypes/template.py
+++ src/interaktiv/templates/contenttypes/template.py
@@ -1,12 +1,14 @@
-from zope.interface import implementer
-from plone.app.contenttypes.interfaces import IDocument
-from plone.app.contenttypes.content import Document
+from zope.interface import implementer, Interface
 from zope import schema
+
+from plone.volto.content import FolderishDocument
+from plone.volto.interfaces import IFolderishDocument
+from plone.autoform import directives
 
 from interaktiv.templates import _
 
 
-class ITemplate(IDocument):
+class ITemplate(Interface):
     """ Interface for Template """
 
     template_description = schema.TextLine(
@@ -16,7 +18,21 @@ class ITemplate(IDocument):
         required=False
     )
 
+    template_thumbnail = schema.TextLine(
+        title=_("Template Thumbnail"),
+        description=_("Choose an thumbnail for the template"),
+        required=False
+    )
+
+    directives.widget(
+        "template_thumbnail",
+        frontendOptions={
+            "widget": "attachedimage",
+            "widgetProps": {"mode": "image", "return": "single"}
+        }
+    )
+
 
 @implementer(ITemplate)
-class Template(Document):
+class Template(FolderishDocument):
     """ Template Container """
