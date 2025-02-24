ab0eecde49f11d57062c62ba9142bd87394ecdee
Maurice Reim <reim@interaktiv.de>
2025-02-24 13:54:08 +0100

    Refactor thumbnail handling logic in subscribers

--- src/interaktiv/templates/subscribers/thumbnail.py
+++ src/interaktiv/templates/subscribers/thumbnail.py
@@ -1,4 +1,4 @@
-from typing import NoReturn
+from typing import NoReturn, Optional
 
 from zope.lifecycleevent import ObjectAddedEvent, ObjectModifiedEvent
 from plone.dexterity.content import DexterityContent
@@ -7,18 +7,33 @@ from Acquisition import aq_base, aq_parent
 from zope.annotation.interfaces import IAnnotations
 from Products.CMFCore.utils import getToolByName
 
+
 def thumbnail_created(obj: DexterityContent, event: ObjectAddedEvent) -> NoReturn:
     if not getattr(obj, "thumbnailUpload", False):
         return
 
     obj.is_template_thumbnail = True
 
+    parent = _get_template_parent(obj)
+    if not parent:
+        return
+
+    _unindex_other_thumbnails(parent, obj)
+
+    parent.template_thumbnail = obj.absolute_url()
+    parent.reindexObject(idxs=["template_thumbnail"])
+
+
+def _get_template_parent(obj: DexterityContent) -> Optional[DexterityContent]:
     parent = aq_parent(obj)
     if parent and parent.portal_type == "Template":
-        catalog = getToolByName(parent, 'portal_catalog')
-        for child in parent.objectValues():
-            if child.is_template_thumbnail and obj.UID() != child.UID():
-                catalog.unindexObject(child)
+        return parent
+    return None
 
-        parent.template_thumbnail = obj.absolute_url()
-        parent.reindexObject(idxs=["template_thumbnail"])
+
+def _unindex_other_thumbnails(parent: DexterityContent, current_obj: DexterityContent) -> None:
+    catalog = getToolByName(parent, 'portal_catalog')
+    for child in parent.objectValues():
+        if getattr(child, "is_template_thumbnail", False) and current_obj.UID() != child.UID():
+            parent.manage_delObjects([child.id])
+            catalog.unindexObject(child)
