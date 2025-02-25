4f73a487c5ff2b6395900119d76e4f67d6ff4502
Maurice Reim <reim@interaktiv.de>
2025-02-25 09:56:54 +0100

    add type hint

--- src/interaktiv/templates/services/types/get.py
+++ src/interaktiv/templates/services/types/get.py
@@ -7,7 +7,7 @@ from interaktiv.templates.helper import get_schema_from_template
 @implementer(IPublishTraverse)
 class InteraktivTemplatesTypesGet(TypesGet):
 
-    def reply_for_type(self):
+    def reply_for_type(self) -> dict:
         schema = super().reply_for_type()
 
         return get_schema_from_template(schema)
