2a62aa6306262722763b7d39d5523d9a9ab4347a
mxurice-rm <reim@interaktiv.de>
2025-03-10 20:16:07 +0100

    Add custom install command to run npm install during setup

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+import subprocess
 import sys
 
 from pkg_resources import Requirement, parse_version
-from setuptools import find_packages, setup
+from setuptools import find_packages, setup, Command
+from setuptools.command.install import install
 
 # Package metadata
 NAME = 'interaktiv.templates'
@@ -32,6 +34,17 @@ def check_python_version():
     if current_version not in required_python:
         sys.exit(f"'{NAME}' requires Python {REQUIRES_PYTHON} but the current Python is {current_version}")
 
+class InstallCommand(Command):
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        print("Running npm install in interaktiv.templates...")
+        subprocess.check_call(['npm', 'install'], cwd='./')
 
 setup(
     name=NAME,
@@ -62,6 +75,9 @@ setup(
     python_requires=check_python_version(),
     install_requires=REQUIRED,
     extras_require=EXTRAS,
+    cmdclass={
+        'install_npm': InstallCommand,
+    },
     entry_points="""
     # -*- Entry points: -*-
     [z3c.autoinclude.plugin]
