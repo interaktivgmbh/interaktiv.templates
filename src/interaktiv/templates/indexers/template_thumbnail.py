0a145c91c02a76dcbe8a36b8587798dec6b4feed
mxurice-rm <reim@interaktiv.de>
2026-02-03 12:40:48 +0100

    normalize string quotes in template_thumbnail indexer

--- src/interaktiv/templates/indexers/template_thumbnail.py
+++ src/interaktiv/templates/indexers/template_thumbnail.py
@@ -1,11 +1,10 @@
-from plone.indexer.decorator import indexer
-
 from interaktiv.templates.contenttypes.template import ITemplate
+from plone.indexer.decorator import indexer
 
 
 @indexer(ITemplate)
 def TemplateThumbnailIndexer(obj: ITemplate) -> str:
     if obj.template_thumbnail:
-        path = '/'.join(obj.getPhysicalPath()[2:])
-        return f'/{path}/@@images/template_thumbnail'
-    return ''
+        path = "/".join(obj.getPhysicalPath()[2:])
+        return f"/{path}/@@images/template_thumbnail"
+    return ""
