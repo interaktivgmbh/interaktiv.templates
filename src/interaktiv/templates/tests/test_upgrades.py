66b42bd0a3b054b2b5e9e6fe508d64a0698801ca
mxurice-rm <reim@interaktiv.de>
2026-02-09 15:56:58 +0100

    normalize string quotes and improve consistency in template_thumbnail index upgrade logic

--- src/interaktiv/templates/tests/test_upgrades.py
+++ src/interaktiv/templates/tests/test_upgrades.py
@@ -10,17 +10,17 @@ class TestUpgrades(TestCase):
     layer = INTEGRATION_TESTING
 
     def test_upgrade_v1020_to_v1030__changes_index_meta_type(self):
-        catalog: CatalogTool = api.portal.get_tool('portal_catalog')
-        catalog.delIndex('template_thumbnail')
-        catalog.addIndex('template_thumbnail', 'BooleanIndex')
+        catalog: CatalogTool = api.portal.get_tool("portal_catalog")
+        catalog.delIndex("template_thumbnail")
+        catalog.addIndex("template_thumbnail", "BooleanIndex")
 
         # precondition
-        index_obj = catalog._catalog.getIndex('template_thumbnail')
-        self.assertEqual(index_obj.meta_type, 'BooleanIndex')
+        index_obj = catalog._catalog.getIndex("template_thumbnail")
+        self.assertEqual(index_obj.meta_type, "BooleanIndex")
 
         # do it
         v1000_to_v1001.upgrade(None)
 
         # postcondition
-        index_obj = catalog._catalog.getIndex('template_thumbnail')
-        self.assertEqual(index_obj.meta_type, 'FieldIndex')
+        index_obj = catalog._catalog.getIndex("template_thumbnail")
+        self.assertEqual(index_obj.meta_type, "FieldIndex")
--- src/interaktiv/templates/upgrades/v1000_to_v1001.py
+++ src/interaktiv/templates/upgrades/v1000_to_v1001.py
@@ -1,4 +1,3 @@
-
 from Products.CMFCore.CatalogTool import CatalogTool
 from Products.GenericSetup.tool import SetupTool
 from plone import api
@@ -6,16 +5,16 @@ from plone import api
 
 # noinspection PyUnusedLocal
 def upgrade(site_setup: SetupTool | None = None) -> None:
-    catalog: CatalogTool = api.portal.get_tool('portal_catalog')
+    catalog: CatalogTool = api.portal.get_tool("portal_catalog")
 
     # change index meta_type to FieldIndex
-    index_obj = catalog._catalog.getIndex('template_thumbnail')
-    if index_obj.meta_type != 'FieldIndex':
-        catalog.delIndex('template_thumbnail')
-        catalog.addIndex('template_thumbnail', 'FieldIndex')
+    index_obj = catalog._catalog.getIndex("template_thumbnail")
+    if index_obj.meta_type != "FieldIndex":
+        catalog.delIndex("template_thumbnail")
+        catalog.addIndex("template_thumbnail", "FieldIndex")
 
     # automatic reindexing for templates
-    brains = catalog(portal_type='Template')
+    brains = catalog(portal_type="Template")
     for brain in brains:
         template_obj = brain.getObject()
         template_obj.reindexObject()
