b953a340f4f7603c9e7416eac57e6e4c4ee95390
mxurice-rm <reim@interaktiv.de>
2026-02-09 15:33:37 +0100

    add upgrade step to update template_thumbnail index to FieldIndex

---
+++ src/interaktiv/templates/upgrades/v1000_to_v1001.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from Products.CMFCore.CatalogTool import CatalogTool
+from Products.GenericSetup.tool import SetupTool
+from plone import api
+
+
+# noinspection PyUnusedLocal
+def upgrade(site_setup: Optional[SetupTool] = None) -> None:
+    catalog: CatalogTool = api.portal.get_tool('portal_catalog')
+
+    # change index meta_type to FieldIndex
+    index_obj = catalog._catalog.getIndex('template_thumbnail')
+    if index_obj.meta_type != 'FieldIndex':
+        catalog.delIndex('template_thumbnail')
+        catalog.addIndex('template_thumbnail', 'FieldIndex')
+
+    # automatic reindexing for templates
+    brains = catalog(portal_type='Template')
+    for brain in brains:
+        template_obj = brain.getObject()
+        template_obj.reindexObject()
